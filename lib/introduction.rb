e43e1005fe8e80fe8b30e7b4ade4a08110df206e
Mary Kanu <maryikanu@gmail.com>
2019-09-30 02:55:14 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,27 +3,19 @@
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
 
+# def introduction(name)
+#  puts "Hi, my name is #{name}."
+#end
+
+#def introduction_with_language(name, language)
+#  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+#end
+
+#def introduction_with_language_optional(name, language="Ruby")
+#  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+#end
 
 
-            # Ruby Method Arguments Lab
-
-# Define a method that takes in and uses an argument
-def introduction(name)
-  # puts "Hi, my name is #{name}."
-  
-end
-
-
-# Define a method that takes in and uses two arguments
-def introduction_with_language(name, language)
- # puts "Hi, my name is #{name} and I am learning to program in #{language}."
- end
- 
- 
-# Define a method with an optional argument
-def introduction_with_language_optional(name, language="Ruby")
- # puts "Hi, my name is #{name} and I am learning to program in #{language}."
-end
 
 
 
